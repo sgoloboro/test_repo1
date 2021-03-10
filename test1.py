ab280bad122f5e71fc323d625bade7684b6c1e2a
sgoloboro <Serezhiny2Hranolosha>
2021-03-09 22:18:11 -0800

    changes was made in branch1_test1 no.6

--- test1.py
+++ test1.py
@@ -3,5 +3,5 @@ import os
 
 # first addition to GitHub.
 msg = "from GitHub!"
-print ("Hi Actions test #1_1 %s " % msg)
-print ("Hi Actions test #2_5 in branch %s " % msg)
+print("Hi Actions test #1_1 %s " % msg)
+print("Hi Actions test #2_5 in branch %s " % msg)
